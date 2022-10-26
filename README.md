0fc9ca0a37e8c664533e44e14acc65fe84a5b708
Andrew Lalis <andrewlalisofficial@gmail.com>
2022-10-26 09:54:58 +0200

    Added starter project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# onion-pong
+# Onion-Pong
 A simple single-player or multi-player variation of the classic "Pong" game.
+
+In Onion Pong, you, the player, control a point in 2D space, which is wrapped in various layers. You're the onion. You control the onion's position with your mouse, and scroll to rotate the player's orientation. One or more balls will spawn, and it'll bounce off the edges of the space, and your layers. However, each time it bounces off of a layer, it breaks a piece off. Layers will grow back slowly over time, and your objective is to survive as long as possible.
+
+This game is written in D and uses **SDL** for graphics and player input, and my own **dvec** library for any linear algebra to be done.
---
+++ source/app.d
@@ -0,0 +1,36 @@
+import std.stdio;
+import bindbc.sdl;
+
+void main() {
+	SDLSupport support = loadSDL();
+	if (support != sdlSupport) {
+		writeln("Couldn't load SDL.");
+		return;
+	}
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		writeln("Couldn't initialize SDL.");
+		return;
+	}
+
+	SDL_Window* window = SDL_CreateWindow("Onion-Pong", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 800, SDL_WINDOW_SHOWN);
+	SDL_Surface* surface = SDL_GetWindowSurface(window);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+	SDL_FillRect(surface, null, SDL_MapRGB((*surface).format, 0xFF, 0x00, 0xFF));
+	SDL_UpdateWindowSurface(window);
+
+	SDL_Event e;
+	bool quit = false;
+	while (!quit) {
+		while (SDL_PollEvent(&e)) {
+			if (e.type == SDL_QUIT) {
+				quit = true;
+				break;
+			}
+		}
+	}
+
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
